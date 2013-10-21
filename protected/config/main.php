273ed12433d1c40ac24cb20f9164bc3175fcfc9c
Jair <dev.lnx1337@gmail.com>
2013-10-21 14:18:28 -0500

    cambios

--- protected/config/main.php
+++ protected/config/main.php
@@ -63,7 +63,7 @@ return array(
 
 
 		// uncomment the following to enable URLs in path-format
-		/*
+		
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'rules'=>array(
@@ -72,7 +72,7 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
+		
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
