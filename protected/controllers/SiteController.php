335aa868260c49a961dd3d708c6d03a77ff70865
Jair <dev.lnx1337@gmail.com>
2013-10-21 14:13:52 -0500

    cambios

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -106,11 +106,10 @@ class SiteController extends Controller
 		Yii::app()->user->logout();
 		$this->redirect(Yii::app()->homeUrl);
 	}
-public function actionOtro(){
-
-
-	echo "hola";
-}
+     
+    public function actionOtro(){
+	     echo "hola";
+    }
 
 	protected function afterRender($view, &$output) {
   parent::afterRender($view,$output);
