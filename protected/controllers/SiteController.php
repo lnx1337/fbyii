ec79f9e9e806fb53ec977f3be89d09fa61be3163
Jair <dev.lnx1337@gmail.com>
2013-10-21 14:37:02 -0500

    cambios

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -112,17 +112,7 @@ class SiteController extends Controller
     public function actionOtro(){
 
 
-	     $userid = Yii::app()->facebook->getUser(); 
-         $loginUrl = Yii::app()->facebook->getLoginUrl();
-
-echo $userid;
-         if($userid){
-         	$results = Yii::app()->facebook->api('/me'); 
-            print_r($results);
-         }else{
-         	         echo '<a href="'.$loginUrl.'" >cambios </a> ';
-
-         }
+	     
          
 
     }
---
+++ protected/views/site/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$userid = Yii::app()->facebook->getUser(); 
+         $loginUrl = Yii::app()->facebook->getLoginUrl();
+
+          echo $userid;
+         if($userid){
+         	$results = Yii::app()->facebook->api('/me'); 
+            print_r($results);
+         }else{
+         	         echo '<a href="'.$loginUrl.'" >cambios </a> ';
+
+         }
+?>
